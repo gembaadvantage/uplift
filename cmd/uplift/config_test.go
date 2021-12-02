78cfe2a6ed5a8ed54ec264f3ca62c223967330b8
paul.t <paul.t@gembaadvantage.com>
2021-12-02 17:27:45 +0000

    increase code coverage for commands

---
+++ cmd/uplift/config_test.go
@@ -0,0 +1,83 @@
+/*
+Copyright (c) 2021 Gemba Advantage
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+package main
+
+import (
+	"io/ioutil"
+	"testing"
+
+	"github.com/gembaadvantage/uplift/internal/git"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestLoadConfig(t *testing.T) {
+	tests := []struct {
+		name     string
+		filename string
+	}{
+		{
+			name:     "DotUpliftYml",
+			filename: ".uplift.yml",
+		},
+		{
+			name:     "DotUpliftYaml",
+			filename: ".uplift.yaml",
+		},
+		{
+			name:     "UpliftYml",
+			filename: "uplift.yml",
+		},
+		{
+			name:     "UpliftYaml",
+			filename: "uplift.yaml",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			git.MkTmpDir(t)
+			upliftConfigFile(t, tt.filename)
+
+			cfg, err := loadConfig()
+
+			require.NoError(t, err)
+			require.Equal(t, "1.0.0", cfg.FirstVersion)
+		})
+	}
+}
+
+func TestLoadConfig_Malformed(t *testing.T) {
+	git.MkTmpDir(t)
+	yml := `firstV`
+	ioutil.WriteFile(".uplift.yml", []byte(yml), 0644)
+
+	_, err := loadConfig()
+	assert.Error(t, err)
+}
+
+func TestLoadConfig_NotExists(t *testing.T) {
+	git.MkTmpDir(t)
+
+	_, err := loadConfig()
+	assert.NoError(t, err)
+}
---
+++ cmd/uplift/util_test.go
@@ -0,0 +1,48 @@
+/*
+Copyright (c) 2021 Gemba Advantage
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+package main
+
+import (
+	"io/ioutil"
+	"testing"
+
+	"github.com/gembaadvantage/uplift/internal/git"
+	"github.com/stretchr/testify/require"
+)
+
+func untaggedRepo(t *testing.T) {
+	t.Helper()
+
+	git.InitRepo(t)
+	git.EmptyCommit(t, "feat: a new feature")
+	require.Len(t, git.AllTags(), 0)
+}
+
+func upliftConfigFile(t *testing.T, name string) {
+	t.Helper()
+
+	yml := "firstVersion: 1.0.0"
+
+	err := ioutil.WriteFile(name, []byte(yml), 0644)
+	require.NoError(t, err)
+}
