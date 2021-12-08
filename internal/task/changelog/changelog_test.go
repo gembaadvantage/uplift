8bda84fbe48a3b447744629e866330a005a99ce3
paul.t <paul.t@gembaadvantage.com>
2021-12-08 06:25:10 +0000

    align changelog template to keep a changelog

--- internal/task/changelog/changelog_test.go
+++ internal/task/changelog/changelog_test.go
@@ -126,6 +126,8 @@ func TestRun_AppendToExistingChangelog(t *testing.T) {
 
 All notable changes to this project will be documented in this file.
 
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
 ## [Unreleased]
 
 ## [1.0.0] - 2021-09-17
@@ -150,6 +152,8 @@ All notable changes to this project will be documented in this file.
 	expected := fmt.Sprintf(`# Changelog
 
 All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
 
@@ -183,6 +187,8 @@ func TestRun_ChangelogEntriesFromFirstTag(t *testing.T) {
 	expected := fmt.Sprintf(`# Changelog
 
 All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
 
