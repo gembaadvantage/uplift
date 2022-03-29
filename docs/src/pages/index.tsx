ceb49b50e3102fa3529aa57a7ca58c8564c02936
paul.t <paul.t@gembaadvantage.com>
2022-03-29 08:27:20 +0100

    restructure pages

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,23 +1,19 @@
-import React from 'react';
 import Head from '@docusaurus/Head';
 import { Redirect } from '@docusaurus/router';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import React from 'react';
 
 export default function HomePage(): JSX.Element {
+  const { siteConfig } = useDocusaurusContext();
   return (
     <>
       <Head>
-        <meta title="Uplift" />
-        <meta
-          property="og:description"
-          content="Semantic versioning the easy way. Powered by Conventional Commits. Built for use with CI"
-        />
-        <meta
-          property="description"
-          content="Semantic versioning the easy way. Powered by Conventional Commits. Built for use with CI"
-        />
+        <meta title={siteConfig.title} />
+        <meta property="og:description" content={siteConfig.tagline} />
+        <meta property="description" content={siteConfig.tagline} />
         <link rel="canonical" href="https://github.com/gembaadvantage/uplift" />
       </Head>
-      <Redirect to="/docs/home/introduction" />
+      <Redirect to="/docs/introduction" />
     </>
   );
 }
