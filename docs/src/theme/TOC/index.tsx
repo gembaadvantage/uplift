ef77005115787f89badba848f28f26fb85a527a0
paul.t <paul.t@gembaadvantage.com>
2022-04-05 05:30:03 +0100

    toc styling

--- docs/src/theme/TOC/index.tsx
+++ docs/src/theme/TOC/index.tsx
@@ -5,7 +5,6 @@ import EditThisPage from '@theme/EditThisPage';
 export default function TOC({ toc, editUrl, ...props }) : JSX.Element {
   return (
     <>
-      <h2>Contents</h2>
       <OriginalTOC toc={toc} {...props} />
       <EditThisPage editUrl={editUrl} />
     </>
