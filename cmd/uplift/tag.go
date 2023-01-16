749850e6a437f4c3624550f31c36501601233036
Paul T <paul.t@gembaadvantage.com>
2023-01-16 08:28:15 +0000

    feat: improve cli documentation when using the --help flag (#303)

--- cmd/uplift/tag.go
+++ cmd/uplift/tag.go
@@ -45,23 +45,45 @@ import (
 )
 
 const (
-	tagDesc = `Tags a git repository with the next semantic version. The tag
-is based on the conventional commit message from the last commit.`
+	tagLongDesc = `Generates a new git tag by scanning the git log of a repository for any
+conventional commits since the last release (or identifiable tag). When
+examining the git log, Uplift will always calculate the next semantic version
+based on the most significant detected increment. Uplift automatically handles
+the creation and pushing of a new git tag to the remote, but this behavior can
+be disabled, to manage this action manually.
 
-	examples = `  # Tag the repository with the next calculated semantic version
-  uplift tag
+Conventional Commits is a set of rules for creating an explicit commit history,
+which makes building automation tools like Uplift much easier. Uplift adheres to
+v1.0.0 of the specification:
 
-  # Identify the next semantic version and write to stdout. 
-  # Repository is not tagged
-  uplift tag --next --silent
+https://www.conventionalcommits.org/en/v1.0.0`
 
-  # Identify the current semantic version and write to stdout. 
-  # Repository is not tagged
-  uplift tag --current
+	tagExamples = `
+# Tag the repository with the next calculated semantic version
+uplift tag
 
-  # Identify the next and current semantic versions and write to stdout.
-  # Repository is not tagged
-  uplift tag --current --next --silent`
+# Identify the next semantic version and write to stdout. Repository is 
+# not tagged
+uplift tag --next --silent
+
+# Identify the current semantic version and write to stdout. Repository 
+# is not tagged
+uplift tag --current
+
+# Identify the current and next semantic versions and write both to stdout.
+# Repository is not tagged
+uplift tag --current --next --silent
+
+# Ensure the calculated version explicitly aheres to the SemVer specification
+# by stripping the "v" prefix from the generated tag
+uplift tag --no-prefix
+
+# Append a prerelease suffix to the next calculated semantic version
+uplift tag --prerelease beta.1
+
+# Tag the repository with the next calculated semantic version, but do not
+# push the tag to the remote
+uplift tag --no-push`
 )
 
 var (
@@ -112,9 +134,9 @@ func newTagCmd(gopts *globalOptions, out io.Writer) *tagCommand {
 
 	cmd := &cobra.Command{
 		Use:     "tag",
-		Short:   "Tag a git repository with the next semantic version",
-		Long:    tagDesc,
-		Example: examples,
+		Short:   "Tag a git repository with the next calculated semantic version",
+		Long:    tagLongDesc,
+		Example: tagExamples,
 		Args:    cobra.NoArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			// If only the current tag is to be printed, skip running a pipeline
